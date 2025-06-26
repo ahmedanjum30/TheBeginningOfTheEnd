d640a8adb0570cfd173adc2745c451da530036da
Ahmad Anjum <ahmedanjum30@gmail.com>
2025-06-26 20:17:53 +0500

    Session Persistence in Native

---
+++ src/firebase.ts
@@ -0,0 +1,32 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  initializeAuth,
+  Auth,
+  getReactNativePersistence,
+} from 'firebase/auth';
+import { Platform } from 'react-native';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+let auth: Auth;
+
+if (Platform.OS === 'web') {
+  auth = getAuth(app);
+} else {
+  auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+  });
+}
+
+export { auth };
