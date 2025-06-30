7b1dcef5fe145eeaccef584635605100007b7d99
Ahmad Anjum <ahmedanjum30@gmail.com>
2025-06-30 20:32:10 +0500

    auth route fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" />
-      <Stack.Screen name="(auth)" />
+      <Stack.Screen name="(non-auth)" />
       <Stack.Screen name="(main)" />
     </Stack>
   );
