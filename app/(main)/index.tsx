e94267dc752e11979a928a3e905399995fb60dcd
Ahmad Anjum <ahmedanjum30@gmail.com>
2025-07-01 23:19:17 +0500

    Gradient Updated

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -1,17 +1,20 @@
 import { View, Text, Button } from 'react-native';
 import { Href, useRouter } from 'expo-router';
+import { GradientBackground } from '@/components/GradientBackground';
 
 export default function Main() {
   const router = useRouter();
   return (
-    <View className="flex-1 justify-center bg-white px-8">
-      <Text className="text-3xl font-bold text-center text-blue-600 mb-8">
-        HI I AM MAIN SCREEN
-      </Text>
-      <Button
-        title="Go to Customers"
-        onPress={() => router.push('/customers' as Href)}
-      />
-    </View>
+    <GradientBackground>
+      <View className="flex-1 justify-center bg-white px-8">
+        <Text className="text-3xl font-bold text-center text-blue-600 mb-8">
+          HI I AM MAIN SCREEN
+        </Text>
+        <Button
+          title="Go to Customers"
+          onPress={() => router.push('/customers' as Href)}
+        />
+      </View>
+    </GradientBackground>
   );
 }
